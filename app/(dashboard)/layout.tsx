86dcc3c3c4e2fd8faa1feb85c53f08652e2c0f23
Pradyut Nair <pradyut.nair@student.uva.nl>
2025-10-14 23:31:43 +0200

    fix theme for dhasboard

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,7 +1,18 @@
+import { ThemeProvider } from "@/components/theme-provider";
 export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <>{children}</>;
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+      storageKey="nexpass-dashboard-theme"
+    >
+      {children}
+    </ThemeProvider>
+  );
 }
