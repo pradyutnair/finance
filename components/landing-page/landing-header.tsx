c1ec5736e621034fe669d139e9deb95ecd522645
Pradyut Nair <pradyut.nair@student.uva.nl>
2025-10-14 12:03:24 +0200

    perfection v3

--- components/landing-page/landing-header.tsx
+++ components/landing-page/landing-header.tsx
@@ -125,7 +125,7 @@ export function LandingHeader() {
               </Link>
             </Button>
 
-            <ThemeToggle />
+            {/* <ThemeToggle /> */}
           </div>
         </div>
       </div>
